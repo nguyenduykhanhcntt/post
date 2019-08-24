d4ce1c63dc77d019b3155b9f4940a00d0be14046
KhanhNguyen <nguyenduykhanhcntt92@gmail.com>
2019-08-24 23:09:13 +0700

    user

---
+++ app/Http/Controllers/LoginController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+class LoginController extends Controller
+{
+	protected $rules = [
+    		'email' =>'required|email',
+    		'password' => 'required|min:6'
+    		];
+
+    protected $messages = [
+    		'email.required' => 'Email là trường bắt buộc',
+    		'email.email' => 'Email không đúng định dạng',
+    		'password.required' => 'Mật khẩu là trường bắt buộc',
+    		'password.min' => 'Mật khẩu phải chứa ít nhất 8 ký tự',
+    	];
+
+
+    public function getLogin()
+    {
+        return view('login');
+    }
+
+    public function postLogin(Request $request)
+    {
+        $email = $request->get('email');
+		$pass = $request->get('password');
+		$this->validate($request,$this->rules, $this->messages);
+
+		if( Auth::attempt(['email' => $email, 'password' =>$pass])) {
+			$data = Auth::User();
+			print_r($data);exit();
+			return view('index' , [ 'data' => $data ]);
+		}else {
+			return 'Tài khoản không tồn tại để đăng nhập <a href="login">Quay Lại</a>';
+		}
+	}
+}
---
+++ resources/views/index.blade.php
@@ -0,0 +1,4 @@
+<?php
+print_r($data);
+    echo 123123;
+?>
